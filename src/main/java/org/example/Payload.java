7f001ce1827fb27ff9eae0f9a306055da6294c48
Aruizmarin <andresrumarin@gmail.com>
2023-01-20 12:39:34 +0100

    Dummy tests
    Creation of mock json file for testing without real api

--- src/main/java/org/example/Payload.java
+++ src/main/java/org/example/Payload.java
@@ -19,4 +19,30 @@ public class Payload {
                 "  \"language\": \"French-IN\"\n" +
                 "}";
     }
+
+    public static String CoursePrice() {
+        return "{\n" +
+                "\t\"dashboard\": {\n" +
+                "\t\t\"purchaseAmount\": 910,\n" +
+                "\t\t\"website\": \"rahulshettyacademy.com\"\n" +
+                "\t},\n" +
+                "\t\"courses\": [\n" +
+                "\t\t{\n" +
+                "\t\t\t\"title\": \"Selenium Python\",\n" +
+                "\t\t\t\"price\": 50,\n" +
+                "\t\t\t\"copies\": 6\n" +
+                "\t\t},\n" +
+                "\t\t{\n" +
+                "\t\t\t\"title\": \"Cypress\",\n" +
+                "\t\t\t\"price\": 40,\n" +
+                "\t\t\t\"copies\": 4\n" +
+                "\t\t},\n" +
+                "\t\t{\n" +
+                "\t\t\t\"title\": \"RPA\",\n" +
+                "\t\t\t\"price\": 45,\n" +
+                "\t\t\t\"copies\": 10\n" +
+                "\t\t}\n" +
+                "\t]\n" +
+                "}";
+    }
 }
