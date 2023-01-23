fc94c0833568bd6486c382320efc5403056ebdd3
Aruizmarin <andresrumarin@gmail.com>
2023-01-23 10:48:14 +0100

    Oauthtest

---
+++ src/main/java/ApiTesting/OAuth20/OAuthTest.java
@@ -0,0 +1,46 @@
+package ApiTesting.OAuth20;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;ç
+import org.openqa.selenium.WebDriver;
+
+import java.sql.SQLOutput;
+
+import static io.restassured.RestAssured.given;
+
+public class OAuthTest {
+    public static void main(String[] args){
+        RestAssured.useRelaxedHTTPSValidation();
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&auth_url=https%3A%2F%2Faccounts.google.com%2Fo%2Foauth2%2Fv2%2Fauth&client_id=692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com&response_type=code&redirect_uri=https%3A%2F%2Frahulshettyacademy.com%2FgetCourse.php&service=lso&o2v=2&flowName=GeneralOAuthFlow");
+        driver.findElement(By.id("identifierId")).sendKeys("srinath19830" + Keys.ENTER);
+        driver.findElement(By.xpath("//input[contains(@type, 'password')]")).sendKeys("password"+ Keys.ENTER);
+        String url = driver.getCurrentUrl();
+        String partialurl = url.split("code=")[1];
+        String code = partialurl.split("&scope")[0];
+        System.out.println(code);
+
+        String accesstokenresponse = given().urlEncodingEnabled(false)
+                .queryParams("code",code)
+                .queryParams("client_id","692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
+                .queryParams("client_secret", "erZOWM9g3UtwNRj340YYaK_W")
+                .queryParams("redirect_uri", "https://rahulshettyacademy.com/getCourse.php")
+                .queryParams("grant_type", "authorization_code")
+                .when().log().all()
+                .post("https://www.googleapis.com/oauth2/v4/token").asString();
+
+        JsonPath js = new JsonPath(accesstokenresponse);
+        String accesstoken = js.getString("access_token");
+
+        String response=given().queryParam("access token",accesstoken)
+                .when().log().all()
+                .get("https://rahulshettyacademy.com/getCourse.php").asString();
+        System.out.println(response);
+    }
+}
